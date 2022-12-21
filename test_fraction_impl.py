6f307eff93da74785c379087fc915cbe1a05a01d
NoeLibon <noe@libon.eu>
2022-12-21 02:19:06 +0100

    test caractéristiques + constructeur

--- test_fraction_impl.py
+++ test_fraction_impl.py
@@ -3,9 +3,27 @@ from fraction_impl import Fraction
 
 
 class FractionTestCase(unittest.TestCase):
+    def test_init(self):
+        with self.assertRaises(ZeroDivisionError):
+            Fraction(num=5, den=0)
+        with self.assertRaises(ZeroDivisionError):
+            Fraction(num=5, den=-0)
+        with self.assertRaises(TypeError):
+            Fraction(num=0.5, den=6)
+        with self.assertRaises(TypeError):
+            Fraction(num=5, den=4.5)
+        with self.assertRaises(TypeError):
+            Fraction(num=-5.3, den=4.5)
+
+    def test_str(self):
+        self.assertEqual(Fraction(num=12, den=3).__str__(), '4')
+        self.assertEqual(Fraction(num=12, den=5).__str__(), '12/5')
+        self.assertEqual(Fraction(num=-12, den=15).__str__(), '-4/5')
+
     def test_as_mixed_number(self):
         self.assertEqual(Fraction(num=12, den=3).as_mixed_number(), '4')
         self.assertEqual(Fraction(num=12, den=5).as_mixed_number(), '2+2/5')
+        self.assertEqual(Fraction(num=12, den=-5).as_mixed_number(), '-2-2/5')
         self.assertEqual(Fraction(num=-12, den=15).as_mixed_number(), '-4/5')
 
     def test_add(self):
@@ -40,12 +58,43 @@ class FractionTestCase(unittest.TestCase):
         self.assertTrue(Fraction(num=4, den=5) == Fraction(num=24, den=30))
         self.assertTrue(Fraction(num=4, den=5) == Fraction(num=-44, den=-55))
         self.assertTrue(Fraction(num=-4, den=5) == Fraction(num=4, den=-5))
+        self.assertFalse(Fraction(num=-4, den=5) == Fraction(num=-4, den=-5))
 
     def test_float(self):
         self.assertEqual(Fraction(num=4, den=5).__float__(), 0.8)
         self.assertEqual(Fraction(num=-40, den=25).__float__(), -1.6)
         self.assertEqual(Fraction(num=-0, den=-144).__float__(), 0)
         self.assertEqual(Fraction(num=144, den=-12).__float__(), -12)
+
+    def test_is_zero(self):
+        self.assertTrue(Fraction(num=0, den=12).is_zero())
+        self.assertFalse(Fraction(num=8, den=7).is_zero())
+        self.assertFalse(Fraction(num=-1, den=5).is_zero())
+        self.assertFalse(Fraction(num=144, den=-12).is_zero())
+
+    def test_is_integer(self):
+        self.assertTrue(Fraction(num=0, den=12).is_integer())
+        self.assertFalse(Fraction(num=8, den=7).is_integer())
+        self.assertFalse(Fraction(num=-1, den=5).is_integer())
+        self.assertTrue(Fraction(num=144, den=-12).is_integer())
+
+    def test_is_proper(self):
+        self.assertTrue(Fraction(num=0, den=12).is_proper())
+        self.assertFalse(Fraction(num=8, den=7).is_proper())
+        self.assertTrue(Fraction(num=-1, den=5).is_proper())
+        self.assertFalse(Fraction(num=144, den=-12).is_proper())
+
+    def test_is_unit(self):
+        self.assertFalse(Fraction(num=0, den=12).is_unit())
+        self.assertFalse(Fraction(num=8, den=7).is_unit())
+        self.assertFalse(Fraction(num=-1, den=5).is_unit())
+        self.assertFalse(Fraction(num=144, den=-12).is_unit())
+
+    def test_is_adjacent_to(self):
+        self.assertTrue(Fraction(num=1, den=2).is_adjacent_to(Fraction(num=1, den=4)))
+        self.assertTrue(Fraction(num=1, den=4).is_adjacent_to(Fraction(num=1, den=2)))
+        self.assertTrue(Fraction(num=-3, den=1).is_adjacent_to(Fraction(num=2, den=-1)))
+        self.assertFalse(Fraction(num=2, den=3).is_adjacent_to(Fraction(num=1, den=4)))
 
 
 if __name__ == '__main__':
